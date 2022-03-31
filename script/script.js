8710c531a3106abd2804a7edd9d1f89ec37c0f53
Sumitskh <93589035+Sumitskh@users.noreply.github.com>
2022-03-31 20:44:46 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -3,8 +3,10 @@ console.log("Hello World");
 // darkmode function to switch the classes dark-mode to body
 function darkMode(){
     let body = document.body;
+    let content1 = document.getElementsByClassName("cont1");
     // console.log("clicked");
-    body.classList.toggle("dark-mode")
+    body.classList.toggle("dark-mode");
+    content1.classList.toggle("dark-mode");
 }
 // Categories dropdown menu
 function dropDown(){
