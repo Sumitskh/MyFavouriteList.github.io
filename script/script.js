25449729da564843eb8e478690e8084d5dd1eecf
Sumitskh <93589035+Sumitskh@users.noreply.github.com>
2022-04-03 19:42:15 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -6,8 +6,10 @@ function darkMode(){
     let content1 = document.getElementsByClassName("cont1");
     // console.log("clicked");
     body.classList.toggle("dark-mode");
-    content1.classList.toggle("dark-mode");
+    // content1.classList.toggle("dark-mode");
 }
+//  dark mode & Local storage
+
 // Categories dropdown menu
 function dropDown(){
     let showDropdown = document.getElementById("myDropdown").classList.toggle("show");
