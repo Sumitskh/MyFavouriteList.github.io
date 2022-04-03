c8f973c269cbb87ee2369e3d18ded5ecba80808b
Sumitskh <93589035+Sumitskh@users.noreply.github.com>
2022-04-03 17:58:54 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -12,3 +12,14 @@ function darkMode(){
 function dropDown(){
     let showDropdown = document.getElementById("myDropdown").classList.toggle("show");
 }
+//  Progress bar
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.body.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
