75e47aeb243b016b6dfab294d84bf64ab1f9cf5c
Sumitskh <93589035+Sumitskh@users.noreply.github.com>
2022-04-15 09:42:06 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -42,3 +42,28 @@ function myburger(){
     cross.style.display = "block";
   }
 }
+// *************** LIVE CLOCK ******************
+setInterval(showtime,0);
+function showtime(){
+  let time = new Date();
+  let hr = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  am_pm = "AM";
+
+  if(hr>12){
+    hr -= 12;
+    am_pm = "PM";
+  }
+  if(hr==0){
+    hr = 12;
+    am_pm = "AM";
+  }
+
+  hr = hr < 10 ? "0" + hr: hr;
+  min = min < 10 ? "0" + min: min;
+  sec = sec < 10 ? "0" + sec: sec;
+
+  let currentTime = hr + ":" + min + ":" + sec + " " + am_pm;
+  document.getElementById("liveclock").innerHTML = currentTime;
+}
